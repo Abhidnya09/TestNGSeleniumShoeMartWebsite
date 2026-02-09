c574034877af1bcc548a3bfa32c18f23108fc394
Abhidnya Patil <139239705+Abhidnya09@users.noreply.github.com>
2026-02-10 00:55:42 +0530

    Made changes in class files

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -7,14 +7,16 @@ import static Tests.SeleniumTests.driver;
 
 
 public class HomePage {
-
+    //Locate the Hamburger icon and Online Products Link
     public static String hamburger_menu_xpath = "//*[@id=\"menuToggle\"]/input";
     public static String onlineProducts_link_xpath = "//*[@id=\"menu\"]/a[3]/li";
 
+    //Click the hamburger icon
     public static void click_hamburger_menu(){
         driver.findElement(By.xpath(hamburger_menu_xpath)).click();
     }
 
+    //Click the order Products link
     public static void click_orderProducts_link() throws InterruptedException {
         Thread.sleep(2000);
         driver.findElement(By.xpath(onlineProducts_link_xpath)).click();
--- src/test/java/Pages/ProductsPages.java
+++ src/test/java/Pages/ProductsPages.java
@@ -1,4 +1,31 @@
 package Pages;
 
 public class ProductsPages {
+    //Locate Shoes Title
+    public static String formalShoes_xpath = "/html/body/div[2]/center/h2";
+    public static String sportsShoes_xpath = "/html/body/div[3]/center/h2";
+    public static String sneakers_xpath = "/html/body/div[4]/center/h2";
+
+    //Locate Shoes Dropdown
+    public static String formalShoesdrpdown_xpath = "/html/body/div[2]/center/div/i[1]";
+    public static String sportsShoesdrpdown_xpath = "/html/body/div[3]/center/div/i[1]";
+    public static String sneakerdrpdown_xpath = "/html/body/div[4]/center/div/i[1]";
+
+    //Locate first shoes name from the list
+    public static String formalShoes_firstshoename = "/html/body/div[2]/table/tbody/tr[1]/td[1]";
+    public static String sportsShoes_firstshoename = "/html/body/div[3]/table/tbody/tr[1]/td[1]";
+    public static String sneakerShoes_firstdhoename="/html/body/div[4]/table/tbody/tr[1]/td[1]";
+
+    public static void formal_shoes_getTitle(){
+
+    }
+
+    public static void sports_shoes_getTitle(){
+
+    }
+
+    public static void sneakers_getTitle(){
+
+    }
+
 }
